7b83cfebf81f73fef79699e8f33143eafb2170fb
Jumpei Usui <jumpeisoccerguitar@gmail.com>
2023-08-15 14:59:22 +0900

    add login

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,19 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@invictus.codes/nuxt-vuetify"],
+  runtimeConfig: {
+    public: {
+      REGION: process.env.REGION,
+      USER_POOL_ID: process.env.USER_POOL_ID,
+      USER_POOL_WEB_CLIENT_ID: process.env.USER_POOL_WEB_CLIENT_ID,
+      DOMAIN: process.env.DOMAIN,
+      REDIRECT_SIGN_IN: process.env.REDIRECT_SIGN_IN,
+      REDIRECT_SIGN_OUT: process.env.REDIRECT_SIGN_OUT,
+    },
+  },
+  vite: {
+    define: {
+      "window.global": {},
+    },
+  },
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,8 @@
+<script setup>
+import { Auth } from "aws-amplify";
+</script>
+
 <template>
-  <v-btn> Button </v-btn>
+  <v-btn @click="Auth.federatedSignIn()">sign in</v-btn>
+  <v-btn @click="Auth.signOut()">sign out</v-btn>
 </template>
