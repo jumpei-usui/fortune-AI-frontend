eda7e0ae4ded8700bd13913aefa30bb99fa3f3cc
Jumpei Usui <jumpeisoccerguitar@gmail.com>
2023-08-20 14:36:56 +0900

    add header to layout

---
+++ components/atoms/Header.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const props = defineProps({ title: String });
+</script>
+
+<template>
+  <v-app-bar color="primary">
+    <v-app-bar-title>{{ props.title }}</v-app-bar-title>
+  </v-app-bar>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const title = "タイトル";
+</script>
+
+<template>
+  <v-app>
+    <atoms-header :title="title"></atoms-header>
+    <v-main>
+      <slot></slot>
+    </v-main>
+  </v-app>
+</template>
